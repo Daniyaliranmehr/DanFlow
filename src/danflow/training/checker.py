3d6b050530ef6bee239786968e353a5a3bff88aa
DaniyalIranMehr <daniyaliranmehr@gmail.com>
2026-08-15 19:01:19 +0330

    feat(checker): Add ForwardCheckResult dataclass

--- src/danflow/training/checker.py
+++ src/danflow/training/checker.py
@@ -10,3 +10,16 @@ from torch.utils.data import DataLoader, random_split
 from tqdm.auto import tqdm
 
 from .trainer import Trainer
+
+
+@dataclass
+class ForwardCheckResult:
+    """
+    Results returned by forward_check().
+    """
+
+    num_batches: int
+    average_loss: float
+    input_shape: tuple[int, ...]
+    target_shape: tuple[int, ...]
+    output_shape: tuple[int, ...]
